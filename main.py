fa9449b14b83b19afc93e89f0bdca30cc8604ca0
mumrbekkk <madatovumrbek8@gmail.com>
2025-11-22 16:48:30 +0500

    logfile conf added & dockerfiles deleted

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import logging
 from core.bot import main
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,  # Set the minimum severity level to log
+        filename='app_log.txt',  # **Crucial: Specifies the output file**
+        filemode='a',  # 'a' appends (adds to the end), 'w' overwrites
+        format='%(asctime)s - %(levelname)s - %(message)s'  # Define the log message format
+    )
     try:
         print("Starting Bakery Bot...")
         asyncio.run(main())
